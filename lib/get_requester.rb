057ae07b01a83e0027af3b058d10c6af23d4eb41
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-29 20:45:30 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,11 +4,13 @@ require 'open-uri'
 class GetRequester
 
   def initialize(url)
-
+    URL = url
   end
 
   def get_response_body
-    
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
   end
 
 end
